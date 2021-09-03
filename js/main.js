139c41ad6fb3b832b8bf81342cc745f323f90f3b
Akuma1991 <abdelrahmangoher1991@gmail.com>
2021-09-03 20:03:53 +0200

    allProjectEdited

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var imgElement = document.getElementById("img-click");
+
+function func1() {
+
+    imgElement.src = "images/portfolio-1.jpg"
+}
+function func2() {
+
+    imgElement.src = "images/portfolio-2.jpg"
+}
+function func3() {
+
+    imgElement.src = "images/portfolio-3.jpg"
+}
+function func4() {
+
+    imgElement.src = "images/portfolio-4.jpg"
+}
+function func5() {
+
+    imgElement.src = "images/portfolio-5.jpg"
+}
+function func6() {
+
+    imgElement.src = "images/portfolio-6.jpg"
+}
